d6c9be3488345dd1b96b472b3ecb5ed3ccde5793
kamiliyap <92785222+kamiliyap@users.noreply.github.com>
2023-03-17 11:57:21 +0700

    Add files via upload

---
+++ Pertemuan 1 pw2/praktik2/variabel.php
@@ -0,0 +1,19 @@
+<?php
+
+    /*
+         
+    */
+    echo $_SERVER["DOCUMENT_ROOT"];
+
+    /*
+    Variable User
+    Variable yang dibuat user
+    */
+    echo '<br></br>';
+
+    $name = 'Kamiliya';
+    $age = 19;
+    $weight = 51;
+    
+    echo "Nama saya adalah " . $name;
+?>
